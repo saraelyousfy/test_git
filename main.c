b75770a53db810faabc9039f7d1985dc2668bed8
saraelyousfy <sara-elyousfy@hotmail.com>
2022-08-18 15:14:25 +0200

    1.0.0

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{int x;
+int y;
+    printf("Hello world!\n");
+    return 0;
+}
---
+++ session11.h
@@ -0,0 +1,7 @@
+#ifndef SESSION11_H_INCLUDED
+#define SESSION11_H_INCLUDED
+
+int sum (int *x,int *y);
+int sub(int *x,int *y);
+
+#endif // SESSION11_H_INCLUDED
